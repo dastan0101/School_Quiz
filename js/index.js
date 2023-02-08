08cb3185940b4a99654f4fcdd909a82b59b120bf
dastan01 <190103323@stu.sdu.edu.kz>
2023-02-08 19:22:20 +0600

    "Initial commit"

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
 const body = document.querySelector("body"),
  toggle = body.querySelector(".toggle");
 
+ var menu = document.getElementsByClassName("menu");
+ for (var i = 0; i < menu.length; i++) {
+    menu[i].addEventListener('click', function() {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    })
+ }
+
 toggle.addEventListener("click", () => {
     body.classList.toggle("dark");
 })
+
