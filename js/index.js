0417c97f0f584fa9fa5cd8d81d96bd0362378e34
dastan0101 <190103323@stu.sdu.edu.kz>
2023-02-16 22:45:46 +0600

    "Added About Us"

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 const body = document.querySelector("body"),
- toggle = body.querySelector(".toggle");
+ toggle = body.querySelector(".toggle"),
+ student_light = body.querySelector(".light"),
+ student_dark = body.querySelector(".dark");
 
  var menu = document.getElementsByClassName("menu");
  for (var i = 0; i < menu.length; i++) {
@@ -12,5 +14,12 @@ const body = document.querySelector("body"),
 
 toggle.addEventListener("click", () => {
     body.classList.toggle("dark");
+    // if (body.classList.contains("dark")) {
+    //     student_dark.style.display = "block";
+    //     student_light.style.display = "none";
+    // } else {
+    //     student_dark.style.display = "none";
+    //     student_light.style.display = "block";
+    // }
 })
 
